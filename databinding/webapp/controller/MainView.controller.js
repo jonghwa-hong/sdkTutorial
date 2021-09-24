bbc2155a95782d17846603dd5504f7159fd066c2
Jonghwa Hong <zzzonghwa@gmail.com>
2021-09-24 21:44:24 +0900

    databinding 02 creating model

--- databinding/webapp/controller/MainView.controller.js
+++ databinding/webapp/controller/MainView.controller.js
@@ -1,15 +1,24 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/model/json/JSONModel",
 ],
 	/**
 	 * @param {typeof sap.ui.core.mvc.Controller} Controller
 	 */
-	function (Controller) {
+	function (Controller, JSONModel) {
 		"use strict";
 
 		return Controller.extend("databinding.controller.MainView", {
 			onInit: function () {
-
+				// object literal을 JSONModel에 전달하고 생성된 JSONModel 인스턴스를 oModel 변수에 저장
+				const oModel = new JSONModel({
+					greetingText: 'Hello World!'
+				});
+				// 현재  view에 model을 설정
+				this.getView().setModel(oModel, 'jsonModel'); 
+				// model data를 text property에 binding
+				const oBindingInfo = {path: 'jsonModel>/greetingText'}
+				this.byId('textbinding').bindText(oBindingInfo);
 			}
 		});
 	});
